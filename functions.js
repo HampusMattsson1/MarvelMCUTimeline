bf861008749d985ca1799c29b9bed8cf07fa0e89
Hampus Mattsson <hjm@ownit.nu>
2024-08-07 19:36:42 +0200

    javascript progress

--- functions.js
+++ functions.js
@@ -8,6 +8,8 @@ async function addPhase(json)
     let row2 = document.getElementById("bottomRow");
 
     let topRow = true;
+    let counter = 0;
+    let lineHeight = "7em";
 
     await Promise.all(json.map(async project => {
         // console.log(project);
@@ -15,67 +17,80 @@ async function addPhase(json)
         let imgUri = "";
 
         try {
-            imgUri = project.imdbcache.image
+            imgUri = project.imdbcache.image;
         }
         catch {
             // console.log("error", project);
         }
-        let div = await createItem(imgUri, project.premiere, project.title);
+        // let div = await createItem(imgUri, project.premiere, project.title, topRow);
 
-        if (topRow)
+        let title = project.title;
+        let date = project.premiere;
+
+        // console.log(imgUri);
+        let div = document.createElement("div");
+        div.className = "item";
+
+        // Fetch image
+        // let response = await fetch(imgUri);
+        // let imageBlob = await response.blob()
+        // let imageObjectURL = URL.createObjectURL(imageBlob);
+
+        let img = document.createElement("img");
+        img.className = "projectImg";
+        img.src = "./img/shehulk.png";
+        // img.src = imageObjectURL;
+        // img.width = "250";
+        img.width = "200";
+        img.height = "100";
+        img.alt = title;
+
+        let line = document.createElement("div");
+        line.className = "projectLine";
+
+        let dateDiv = document.createElement("div");
+        dateDiv.className = "projectDate";
+
+        let datetime = new Date(date);
+
+        // Lägg till år
+        let year = datetime.getFullYear();
+        years.add(year);
+        yearWidth[year] = yearWidth[year] ? yearWidth[year] += 1 : 1;
+        // console.log(year, yearWidth[year]);
+
+        // dateDiv.innerText = datetime.toLocaleString('en-us', { month: 'long' });
+        dateDiv.innerText = datetime.toLocaleString('en-us', { month: 'short' }) + " " + year;
+
+        // Long / kort linje
+        line.style.height = lineHeight;
+
+        if (counter % 2 == 0)
         {
+            div.appendChild(img);
+            div.appendChild(line);
+            div.appendChild(dateDiv);
+
             row1.appendChild(div);
+
+            if (lineHeight == "3em")
+            {
+                lineHeight = "7em";
+            }
+            else
+            {
+                lineHeight = "3em";
+            }
+
         } else {
+            div.appendChild(dateDiv);
+            div.appendChild(line);
+            div.appendChild(img);
+
             row2.appendChild(div);
         }
 
         topRow = !topRow;
-
+        counter++;
     }));
-}
-
-async function createItem(imgUri, date, title)
-{
-    // console.log(imgUri);
-    let div = document.createElement("div");
-    div.className = "item";
-
-    // Fetch image
-    // let response = await fetch(imgUri);
-    // let imageBlob = await response.blob()
-    // let imageObjectURL = URL.createObjectURL(imageBlob);
-
-    let img = document.createElement("img");
-    img.className = "projectImg";
-    img.src = "./img/shehulk.png";
-    // img.src = imageObjectURL;
-    // img.width = "250";
-    img.width = "200";
-    img.height = "100";
-    img.alt = title;
-
-    div.appendChild(img);
-
-    let line = document.createElement("div");
-    line.className = "projectLine";
-
-    div.appendChild(line);
-
-    let dateDiv = document.createElement("div");
-    dateDiv.className = "projectDate";
-
-    let datetime = new Date(date);
-
-    // Lägg till år
-    let year = datetime.getFullYear()
-    years.add(year);
-    yearWidth[year] = yearWidth[year] ? yearWidth[year] += 1 : 1;
-    // console.log(year, yearWidth[year]);
-
-    // dateDiv.innerText = datetime.toLocaleString('en-us', { month: 'long' });
-    dateDiv.innerText = datetime.toLocaleString('en-us', { month: 'short' }) + " " + year;
-
-    div.appendChild(dateDiv);
-
-    return div;
 }
